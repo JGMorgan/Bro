0b785f0ec330522cdc02915faf368e039681475a
JGMorgan <josegm11194@gmail.com>
2016-08-06 18:51:19 -0400

    neural network is not quite there yet but we're getting there

--- bro_cli/main.go
+++ bro_cli/main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
-	//	"bufio"
+	"bufio"
 	"fmt"
-	//	"os"
+	"net/http"
+	"os"
+	"strings"
 )
+
+type BroList struct {
+	bros []string
+}
 
 func main() {
 	fmt.Println("Welcome to Bro CLI\nType help to get a list of valid commands")
@@ -14,7 +20,29 @@ func main() {
 func menu() {
 	for {
 		var s string
-		fmt.Scanln(&s)
-		fmt.Println(s)
+		scnr := bufio.NewScanner(os.Stdin)
+		done := scnr.Scan()
+		if done {
+			s = scnr.Text()
+		}
+		if strings.EqualFold(s, "help") {
+			showCommands()
+		}
+		if strings.EqualFold(s, "brolist") {
+			showBroList()
+		}
 	}
+}
+
+func showCommands() {
+	fmt.Println("register --create a new bro account\n" +
+		"login [username] [password] --already have a Bro account? Just Login\n" +
+		"add [bro_name] --add a new bro\n" +
+		"brolist --show a list of all your bros\n" +
+		"bro [bro_name] --open a chat with one of your bros\n")
+}
+
+func showBroList() {
+	resp, err := http.Get("") //get list
+	return resp
 }
--- bro_cli/neural_network.go
+++ bro_cli/neural_network.go
@@ -1,5 +1,32 @@
 package neural_net
 
-func sigmoid(x float64) {
-	return (1 / (1 + (1.6 * *x)))
+import (
+	"math"
+)
+
+type NeuralNet struct {
+	NumInputs       int
+	HiddenLayers    int
+	NeuronsPerLayer int //placeholder this will layer be an array of ints
+	W1              mat64.Dense
+	W2              mat64.Dense //currently only 2 weight layers this will be changed if time allows
+}
+
+func CreateNeuralNet(numInputs int, numHiddenLayers int, numNeuronsPerLayer int) *NeuralNet {
+	return &NeuralNet{numInputs, numHiddenLayers, numNeuronsPerLayer, mat64.NewDense(numInputs, numNeuronsPerLayer, nil), mat64.NewDense(numNeuronsPerLayer, 1, nil)}
+}
+
+func Predict(X []float64) float64 {
+	X2 := [][]float64{}
+	X2 = append(X2, X)
+	inputMatrix := mat64.NewDense(1, len(X), X2)
+
+}
+
+func Sigmoid(x float64) float64 {
+	return (1 / (1 + math.Pow(math.E, -x)))
+}
+
+func SigmoidPrime(x float64) float64 {
+	return (math.Pow(math.E, -x) / math.Pow((1+math.Pow(math.E, -x)), 2))
 }
